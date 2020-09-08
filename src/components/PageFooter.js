83d42fec47317655a920082963d61275b01ccb07
manpenaloza <manuel.penaloza@gmx.at>
2020-09-08 12:15:11 +0200

    fix legal issues

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,12 +4,9 @@ export default function PageFooter() {
   return (
     <div id="footer">
       <ul className="copyright">
-        <li>&copy; Untitled. All rights reserved.</li>
+        <li>&copy; Manuel Penaloza. All rights reserved.</li>
         <li>
-          Author: <a href="https://manpenaloza.dev">Manuel Penaloza</a>
-        </li>
-        <li>
-          Demo Images: <a href="http://unsplash.com/">Unsplash</a>
+          Images: <a href="http://unsplash.com/">Unsplash</a>
         </li>
       </ul>
     </div>
