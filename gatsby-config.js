8074623a31bcb9fe06a0ad7f72795f728296317a
manpenaloza <manuel.penaloza@gmx.at>
2020-09-08 16:03:48 +0200

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,25 @@ module.exports = {
         icon: config.manifestIcon, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-138398009-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields,
+        cookieDomain: "manpenaloza.dev",
+      }
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
